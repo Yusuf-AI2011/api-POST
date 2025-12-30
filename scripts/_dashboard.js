4100b0167e1b401bef1401c2d920ef91a0b16488
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2025-12-30 11:32:19 +0500

    users

--- scripts/_dashboard.js
+++ scripts/_dashboard.js
@@ -1,5 +1,3 @@
-const buttonAn = document.querySelector(".head__buttonAn");
-const buttonPro = document.querySelector(".head__buttonPro");
 const buttonLogout = document.querySelector(".head__button");
 
 buttonLogout.addEventListener("click", () => {
@@ -7,16 +5,14 @@ buttonLogout.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
-buttonAn.addEventListener("click", () => {
-  // e.preventDefault();
-  document.querySelector(".an__wrapper").style.cssText = "display: flex;";
-  document.querySelector(".products__wrapper").style.cssText = "display: none;";
-});
-
-buttonPro.addEventListener("click", () => {
-  // e.preventDefault();
-  document.querySelector(".an__wrapper").style.cssText = "display: none;";
-  document.querySelector(".products__wrapper").style.cssText = "display: flex;";
-});
-
-console.log(window.location.href);
+let href = window.location.href;
+if (href == "http://127.0.0.1:5500/carts.html") {
+  document.querySelectorAll(".head__card")[5].style.cssText =
+    "box-shadow: 0px 0px 10px 0px #fff;";
+} else if (href == "http://127.0.0.1:5500/users.html") {
+  document.querySelectorAll(".head__card")[2].style.cssText =
+    "box-shadow: 0px 0px 10px 0px #fff;";
+} else if (href == "http://127.0.0.1:5500/dashboard.html") {
+  document.querySelectorAll(".head__card")[1].style.cssText =
+    "box-shadow: 0px 0px 10px 0px #fff;";
+}
