b542f51b0dc28b272e511ba8e24e4ff6b0706556
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2025-12-31 11:01:00 +0500

    fixing

--- scripts/_dashboard.js
+++ scripts/_dashboard.js
@@ -8,13 +8,22 @@ buttonLogout.addEventListener("click", () => {
 let href = window.location.href;
 console.log(href);
 
-if (href == "http://127.0.0.1:5500/carts.html") {
+if (
+  href == "http://127.0.0.1:5500/carts.html" ||
+  href == "https://api-post-ten.vercel.app/carts.html"
+) {
   document.querySelectorAll(".head__card")[5].style.cssText =
     "box-shadow: 0px 0px 10px 0px #fff;";
-} else if (href == "http://127.0.0.1:5500/users.html") {
+} else if (
+  href == "http://127.0.0.1:5500/users.html" ||
+  href == "https://api-post-ten.vercel.app/users.html"
+) {
   document.querySelectorAll(".head__card")[2].style.cssText =
     "box-shadow: 0px 0px 10px 0px #fff;";
-} else if (href == "http://127.0.0.1:5500/dashboard.html") {
+} else if (
+  href == "http://127.0.0.1:5500/dashboard.html" ||
+  href == "https://api-post-ten.vercel.app/dashboard.html"
+) {
   document.querySelectorAll(".head__card")[1].style.cssText =
     "box-shadow: 0px 0px 10px 0px #fff;";
 }
